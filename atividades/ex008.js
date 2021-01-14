de2c00b8c6bce51621eca2dc64bbb031b79fca2d
adrianaVlongo <66226590+AdrianaViabL@users.noreply.github.com>
2021-01-14 13:14:05 -0300

    ate a aula 11
    atividades feitas ate a aula 11 do Curso em video

---
+++ atividades/ex008.js
@@ -0,0 +1,9 @@
+//aula 11
+console.log('exbiu o texto?')
+var vel = 50.6
+console.log(`A velocidade do seu carro é ${vel}`)
+
+if (vel > 60){  //condicional simples
+    console.log(`Voce ultrapassou a velocidade limite!`)
+}
+console.log(`dirija sempre usando o cinto de segurança!`)
---
+++ atividades/ex009.js
@@ -0,0 +1,8 @@
+//aula 11
+var pais = 'Brasil'
+console.log(`vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log(`Voce é Brasileiro!`)
+}else{
+    console.log(`Voce é Estrangeiro!`)
+}
